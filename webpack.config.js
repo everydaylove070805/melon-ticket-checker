1ef5c66dac37d02cd0898bc588c3762394aef415
ShuJungKuo <62538180+ShuJungKuo@users.noreply.github.com>
2025-03-07 11:02:47 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,9 @@
-const path = require('path');
-
+// webpack.config.js
 module.exports = {
-  entry: './src/index.js', // 你的入口文件
+  entry: './src/index.js', // 設定入口點
   output: {
-    filename: 'bundle.js', // 輸出的文件名稱
-    path: path.resolve(__dirname, 'dist') // 輸出文件的目錄
+    filename: 'bundle.js', // 設定輸出文件名
+    path: __dirname + '/dist', // 輸出目錄
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/, // 處理所有 js 文件
-        exclude: /node_modules/,
-        use: 'babel-loader' // 使用 babel-loader 處理 JS 文件
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js'] // 設定支持的擴展名
-  }
+  mode: 'development', // 設定 mode，開發模式
 };
