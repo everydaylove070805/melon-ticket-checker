ea74693f36821a87c3505c7bae5bf14799c15e33
ShuJungKuo <62538180+ShuJungKuo@users.noreply.github.com>
2025-03-07 10:54:11 +0800

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,14 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = [
   {
-    // 直接加入你要繼承的配置
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: "module",
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
     },
     rules: {
       "no-console": "warn",
-    },
-  },
-  {
-    // 如果你需要更多的共享配置，將它直接放在此處
-    parserOptions: {
-      ecmaVersion: "latest",
     },
   },
 ];
